b7d7a9f4c4d11a3da4efdaa7a7180f462fee5505
DmitriyDubravin <dmitriy.dubravin@gmail.com>
2018-06-18 20:34:58 +0300

    fn with args

--- binder.js
+++ binder.js
@@ -1,7 +1,7 @@
 
-// v.2.1
+// v.2.2
 
-export default function binder(selectorsAndFunctionsBounds, runTests = false) {
+export function binder(selectorsAndFunctionsBounds, runTests = false) {
     let t0, t1;
     if (runTests) t0 = performance.now();
     // polyfill for ".matches()" method
@@ -36,7 +36,14 @@ export default function binder(selectorsAndFunctionsBounds, runTests = false) {
                 }
             });
         } else if (nature === "[object Object]") {
-            mergedModules = Object.assign(mergedModules, module);
+            if (module.f !== undefined) {
+                // call function with arguments
+                mergedModules[module.f.name] = module.f;
+                mergedModules[module.f.name](...module.a);
+            } else {
+                // or just merge object
+                mergedModules = Object.assign(mergedModules, module);
+            }
         } else if (nature === "[object Function]") {
             mergedModules[module.name] = module;
             mergedModules[module.name]();
@@ -48,6 +55,61 @@ export default function binder(selectorsAndFunctionsBounds, runTests = false) {
     if (runTests) t1 = performance.now();
     if (runTests) console.log("Binder html parsing took " + (t1 - t0) + " milliseconds.");
 }
+
+// for function with arguments
+export const fwa = (...args) => ({f: args.shift(), a: args});
+
+
+
+// v.2.1
+
+// export default function binder(selectorsAndFunctionsBounds, runTests = false) {
+//     let t0, t1;
+//     if (runTests) t0 = performance.now();
+//     // polyfill for ".matches()" method
+//     if (!Element.prototype.matches) {
+//         Element.prototype.matches = Element.prototype.msMatchesSelector;
+//     }
+//     // gather all selectors in array
+//     const selectorsToFind = Object.keys(selectorsAndFunctionsBounds);
+//     // find selectors in document
+//     const foundElements = [...document.querySelectorAll(selectorsToFind.join(","))];
+//     // filter bounds for not founded selectors
+//     let filteredBounds = {};
+//     for (let key in selectorsAndFunctionsBounds) {
+//         if (foundElements.some(element => element.matches(key))) {
+//             filteredBounds[key] = selectorsAndFunctionsBounds[key];
+//         } else {
+//             if (runTests) console.log(`- ${key} was not found`);
+//         }
+//     }
+//     // gather all modules in one object
+//     let mergedModules = {};
+//     for (let bound in filteredBounds) {
+//         let module = filteredBounds[bound];
+//         let nature = Object.prototype.toString.call(module);
+//         if (nature === "[object Array]") {
+//             module.forEach(script => {
+//                 if (Object.prototype.toString.call(script) === "[object Function]") {
+//                     mergedModules[script.name] = script;
+//                     mergedModules[script.name]();
+//                 } else {
+//                     mergedModules = Object.assign(mergedModules, script);
+//                 }
+//             });
+//         } else if (nature === "[object Object]") {
+//             mergedModules = Object.assign(mergedModules, module);
+//         } else if (nature === "[object Function]") {
+//             mergedModules[module.name] = module;
+//             mergedModules[module.name]();
+//         } else {
+//             console.log("! unsupported format: ", module);
+//         }
+//     }
+//     if (runTests) console.log("binderResultObject: ", mergedModules);
+//     if (runTests) t1 = performance.now();
+//     if (runTests) console.log("Binder html parsing took " + (t1 - t0) + " milliseconds.");
+// }
 
 
 
